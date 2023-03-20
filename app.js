5ef84b463cffad31fbb279aa68ca41fc71e73b19
PopaSerban <serban.popa@outlook.com>
2023-03-20 11:43:30 +0200

    Campground New and Create
    Added Campground creation and new page

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true}));
+
 app.get('/', (req, res) => {
     console.log('hello from camping app!');
     res.render('home');
@@ -25,6 +27,15 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', async (req, res)=> {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds: campgrounds});
+});
+
+app.get('/campgrounds/new', async (req, res) =>{
+    res.render('campgrounds/new');
+});
+app.post('/campgrounds', async (req,res) => {
+     const campground = new Campground(req.body.campground)
+     await campground.save();
+     res.redirect(`/campgrounds/${campground._id}`)
 });
 
 app.get('/campgrounds/:id', async (req, res) => {
