98b0b558657a2143955e1a23e0ec13dad215ca68
PopaSerban <serban.popa@outlook.com>
2023-03-23 23:10:02 +0200

    Added photos and description for campgrounds
    Added image property in campground model
    Seed now adds image for each campground with unsplash photos api

--- seeds/index.js
+++ seeds/index.js
@@ -19,9 +19,13 @@ mongoose.connect('mongodb://localhost:27017/campBooking', {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random()*1000);
+        const price = Math.floor(Math.random()*20)+10;
         const camp =  new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione animi, architecto unde provident est, numquam optio eligendi modi facere natus rem aliquid! Nostrum vel nemo, ullam quas excepturi voluptate iure.',
+            price: price
         })
         console.log(`seeded ${i} campgrounds`);
         await camp.save();
